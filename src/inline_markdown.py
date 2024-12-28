7d2a034da596d10788f3bb9bd6a24b65c6199d92
kindiregg <jpriveraa7@gmail.com>
2024-12-28 14:14:24 -0600

    split links and images into text nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -23,3 +24,71 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        images = extract_markdown_images(node.text)
+        if not images:
+            new_nodes.append(node)
+            continue
+        
+        split_nodes = []
+        current_text = node.text
+        for image_alt, image_link in images:
+            sections = current_text.split(f"![{image_alt}]({image_link})", 1)
+
+            if sections[0]:
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+
+            current_text = sections[1] if len(sections) > 1 else ""
+        if current_text:
+            split_nodes.append(TextNode(current_text, TextType.TEXT))
+
+        new_nodes.extend(split_nodes)
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        links = extract_markdown_links(node.text)
+        if not links:
+            new_nodes.append(node)
+            continue
+            
+        split_nodes = []
+        current_text = node.text
+        for link_text, link_url in links:
+            sections = current_text.split(f"[{link_text}]({link_url})", 1)
+            
+            if sections[0]:
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            
+            current_text = sections[1] if len(sections) > 1 else ""
+        
+        if current_text:
+            split_nodes.append(TextNode(current_text, TextType.TEXT))
+            
+        new_nodes.extend(split_nodes)
+        
+    return new_nodes
+
+
